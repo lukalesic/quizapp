4e3f0fd7e63f9132cfe946a1f5da85e603ff094f
Marino Ladis <marinoladis@hotmail.com>
2022-12-06 11:53:01 +0100

    changed style of homescreen

--- lib/style/appstyle.dart
+++ lib/style/appstyle.dart
@@ -2,35 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppStyle {
-
-  static Color  backgroundColor = Color(0xFFe2e2fe);
+  static Color backgroundColor = Color(0xFF171819);
   static Color mainColor = Color(0xFF000633);
-  static Color accentColor = Color(0xFF0065FF);
+  static Color accentColor = Color(0xFF039CFD);
+  static Color textColor = Color(0xFFCACACA);
+  static Color borderColor = Color(0x70CACACA);
 
 //colors for correct and incorrect answers
   static Color correct = Color(0xFF32AB58);
   static Color incorrect = Color(0xFFAB3232);
 
   static List<Color> cardsColor = [
-  Colors.white,
-  Colors.red.shade100,
-  Colors.pink.shade100,
-  Colors.orange.shade100,
-  Colors.yellow.shade100,
-  Colors.green.shade100,
-  Colors.blue.shade100,
-  Colors.blueGrey.shade100,
-];
-
+    Colors.white,
+    Colors.red.shade100,
+    Colors.pink.shade100,
+    Colors.orange.shade100,
+    Colors.yellow.shade100,
+    Colors.green.shade100,
+    Colors.blue.shade100,
+    Colors.blueGrey.shade100,
+  ];
 
   static TextStyle mainTitle = GoogleFonts.roboto(
-    fontSize: 18,
-    fontWeight: FontWeight.bold
-  );
+      fontSize: 18, fontWeight: FontWeight.bold, color: textColor);
 
-   static TextStyle mainContent = GoogleFonts.nunito(
-    fontSize: 16,
-    fontWeight: FontWeight.normal
-  );
-
+  static TextStyle mainContent = GoogleFonts.nunito(
+      fontSize: 16, fontWeight: FontWeight.normal, color: textColor);
 }
