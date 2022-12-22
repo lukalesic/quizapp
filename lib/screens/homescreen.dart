e6bf5209699523121c1f0f21bab185e101f7af9d
Marino Ladis <marinoladis@hotmail.com>
2022-12-22 13:52:10 +0100

    reading request as utf8 to fix strange characters

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -11,7 +11,8 @@ Future<Questions?> fetchQuestions() async {
       .get(Uri.parse('https://mdfjfuhfct.eu-west-1.awsapprunner.com/quiz/'));
 
   if (response.statusCode == 200) {
-    Questions questions = Questions.fromJson(jsonDecode(response.body));
+    Questions questions =
+        Questions.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
     for (var question in questions.questions) {
       question.movies.shuffle();
     }
